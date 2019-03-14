42869d85b1b3d7b09f2b4d0c5dffeaef5a9ec4d4
hanane <nam.hana@yahoo.fr>
2019-03-14 15:41:19 +0100

    afficher le tableau des clients

--- js/main.js
+++ js/main.js
@@ -1,35 +1,25 @@
+var tbody = document.getElementById("tbody");
 // instanciation de XMLHttpRequest
 var xhttp = new XMLHttpRequest();
 // creation de la fonction qui recupere le fichier json et afficher le contenu
-xhttp.onreadystatechange = function(clientsListe) {
+xhttp.onreadystatechange = function(id, nom, age, ville) {
     if (this.readyState == 4 && this.status == 200) {
       //recuperation de l'objet sous format json
       var clientsListe = JSON.parse(this.responseText);
       // stocker le tableau dans la variable clients
       var clients = clientsListe.clients;
       //on parcour le tableau des clients pour recuperer tout l'objet
-      for (let index = 0; index < clients.length; index++) {
-        // on stock le tableau d'un seul client dans la variable client
-        var client = clients[index]; 
-        // on parcours les proprietes d'un client       
-        for (property in client) {
-          balise();
-          // remplir les champs des balises de la table
-          id.innerHTML = client.id + "<br>";            
-          nom.innerHTML = client.nom + "<br>";
-          age.innerHTML = client.age + "<br>";
-          ville.innerHTML = client.ville + "<br>";
-          console.log(client[property]);
-        }      
+      for (let i = 0; i < clients.length; i++) {
+        var tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        for (attribut in clients[i]) {
+          var td = document.createElement("td");
+          td.innerHTML = clients[i][attribut];
+          tr.appendChild(td);
+
+        }
       }
     }
-  };
+};
   xhttp.open("GET", "clients.json", true);
   xhttp.send();
-function balise() {
-  // fonction qui recupere des balise de la table
-  var id = document.getElementById("id")
-  var nom = document.getElementById("nom");
-  var age = document.getElementById("age");
-  var ville = document.getElementById("ville");    
-}
