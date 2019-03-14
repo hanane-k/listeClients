2552b3d450a57fdd4903aa3669eb447544e9535f
hanane <nam.hana@yahoo.fr>
2019-03-14 10:15:54 +0100

    recuperation d'un seule client

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
-var clients = document.getElementById("clients");
+// instanciation de XMLHttpRequest
 var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function(clientsListe) {
+// creation de la fonction qui recupere le fichier json et afficher le contenu
+xhttp.onreadystatechange = function(clientsListe) {
     if (this.readyState == 4 && this.status == 200) {
+      //recuperation de l'objet sous format json
       var clientsListe = JSON.parse(this.responseText);
-      for (let i = 0; i < clientsListe.length; i++) {
-      var id = document.getElementById("id");
-      id.innerHTML = clientsListe.id + "<br>";            
-      var nom = document.getElementById("nom");
-      nom.innerHTML = clientsListe.nom + "<br>";
-      var age = document.getElementById("age");
-      age.innerHTML = clientsListe.age + "<br>";
-      var ville = document.getElementById("ville");
-      ville.innerHTML = clientsListe.ville + "<br>";
+      // stocker le tableau dans la variable clients
+      var clients = clientsListe.clients;
+      //on parcour le tableau des clients pour recuperer tout l'objet
+      for (let index = 0; index < clients.length; index++) {
+        // on stock le tableau d'un seul client dans la variable client
+        var client = clients[index]; 
+        // on parcours les proprietes d'un client       
+        for (property in client) {
+          balise();
+          // remplir les champs des balises de la table
+          id.innerHTML = client.id + "<br>";            
+          nom.innerHTML = client.nom + "<br>";
+          age.innerHTML = client.age + "<br>";
+          ville.innerHTML = client.ville + "<br>";
+          console.log(client[property]);
+        }      
       }
     }
-    // console.log(JSON.parse(this.responseText));    
-    console.log(clientsListe.ville);    
   };
   xhttp.open("GET", "clients.json", true);
-  xhttp.send(); 
-  
+  xhttp.send();
+function balise() {
+  // fonction qui recupere des balise de la table
+  var id = document.getElementById("id")
+  var nom = document.getElementById("nom");
+  var age = document.getElementById("age");
+  var ville = document.getElementById("ville");    
+}
